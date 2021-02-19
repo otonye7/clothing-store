c1f317acbacb60cdb55c3fe14b448334a7f69f4a
otonye <otonyeamie@gmail.com>
2021-02-19 23:04:33 +0100

    master

---
+++ src/components/menu-items/menu-items.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '../../components/header/header.component';
+import { MenuItemContainer } from './men-new-arrival.styles';
+
+
+const MenuItem = () => {
+
+	return ( 
+         <MenuItemContainer>
+             <div className='headers'>
+               
+              
+            </div>
+            </MenuItemContainer>
+           
+	);
+}; 
+
+
+export default MenuItem;
