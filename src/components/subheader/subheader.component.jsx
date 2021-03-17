929f7a2b59f5279c220ae34e81577869a3f5f717
otonye <otonyeamie@gmail.com>
2021-03-17 21:07:11 +0100

    master

--- src/components/subheader/subheader.component.jsx
+++ src/components/subheader/subheader.component.jsx
@@ -4,288 +4,46 @@ import { Link } from 'react-router-dom';
 
 
 const SubHeader = ({open}) => {
-   console.log(open)
+  
     return(
         <SubHeaderContainer open={open} >
-                 
-                 <div className ='new-arrival' >
-                     <Link className='nav-links'>
-                          NEW ARRIVAL
-                     </Link>
-
-                <div className='sub-links'>
-
-                 <div className='links-container'>
-                     <ul>
-                        <div  className='nav-link'>MEN'S ARRIVALS</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>WOMEN'S ARRIVALS</div>
-                    </ul>
-                    <ul>
-                        <div  className='nav-link'>SHOES</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>SOREL ARRIVAL</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>BUGABOO ARRIVAL</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>NIKE ARRIVAL</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>COLUMBIA ARRIVAL</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>ADIDAS ARRIVAL</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>CONVERSE ARRIVAL</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>PUMA ARRIVAL</div>
-                    </ul>
-                 </div>
-                 
-                <div className='image-container'>
-                     <div className='image'></div>
-
-                <div className='shop'>
-                   <Link  to='/new-arrival' className='shop-links'>SHOP NOW</Link>
-                </div>
-
-                </div>
-                </div>
-                </div>
-
-                <div className ='new-arrival' >
-                     <Link className='nav-links'>
-                          MEN
-                     </Link>
-                     
-                <div className='sub-links'>
-
-                 <div className='links-container'>
-                     <ul>
-                        <div  className='nav-link'>HERITAGE</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>LIFESTYLE</div>
-                    </ul>
-                    <ul>
-                        <div  className='nav-link'>SKATES</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>BASKETBALL</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>TENNIS</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>SOCCER</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>BOOTS</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>TRACKSUITS</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>SHORTS</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>SHIRTS</div>
-                    </ul>
-                 </div>
-                 
-                <div className='image-container'>
-                     <div className='second-image'></div>
-
-                <div className='shop'>
-                   <Link className='shop-links'>SHOP NOW</Link>
-                </div>
-                
-                </div>
-                </div>
-              </div>
-
-                 <div className ='new-arrival'>
-                     <Link value = 'WOMEN' className='nav-links'>
-                          WOMEN
-                     </Link>
-                     
-                       <div className='sub-links'>
-
-                 <div className='links-container'>
-                     <ul>
-                        <div  className='nav-link'>HERITAGE</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>LIFESTYLE</div>
-                    </ul>
-                    <ul>
-                        <div  className='nav-link'>SKATES</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>BASKETBALL</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>TENNIS</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>SOCCER</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>BOOTS</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>TRACKSUITS</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>SHORTS</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>SHIRTS</div>
-                    </ul>
-                 </div>
-                 
-                <div className='image-container'>
-                     <div className='third-image'></div>
-
-                <div className='shop'>
-                   <Link className='shop-links'>SHOP NOW</Link>
-                </div>
-                
-                </div>
-                </div>
-                </div>
-
-                 <div className ='new-arrival' >
-                     <Link value = 'KIDS' className='nav-links'>
-                          KIDS
-                     </Link>
-                       <div className='sub-links'>
-
-                 <div className='links-container'>
-                     <ul>
-                        <div  className='nav-link'>HERITAGE</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>LIFESTYLE</div>
-                    </ul>
-                    <ul>
-                        <div  className='nav-link'>SKATES</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>BASKETBALL</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>TENNIS</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>SOCCER</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>BOOTS</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>TRACKSUITS</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>SHORTS</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>SHIRTS</div>
-                    </ul>
-                 </div>
-                 
-                <div className='image-container'>
-                     <div className='fourth-image'></div>
-
-                <div className='shop'>
-                   <Link className='shop-links'>SHOP NOW</Link>
-                </div>
-                
-                </div>
-                </div>
-                 </div>
-
-                 <div className ='new-arrival' >
-                     <Link className='nav-links'>
-                          LIFESTYLES
-                     </Link>
-                 </div>
-
-                 <div className ='new-arrival'>
-                     <Link className='nav-links'>
-                          SALES
-                     </Link>
-                 </div>
-
-                 <div className ='new-arrival' >
-                     <Link className='nav-links'>
-                          BRANDS
-                     </Link>
-                       <div className='sub-links'>
-
-                 <div className='links-container'>
-                     <ul>
-                        <div  className='nav-link'>GUCCI</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>ADIDAS</div>
-                    </ul>
-                    <ul>
-                        <div  className='nav-link'>LACOSTE</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>ROYAL CROWN</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>BUGABOO</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>PUMA</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>NIKE</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>JORDAN</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>NEW BALANCE</div>
-                    </ul>
-                    <ul>
-                        <div className='nav-link'>CONVERSE</div>
-                    </ul>
-                 </div>
-                 
-                <div className='image-container'>
-                     <div className='fifth-image'></div>
-
-                <div className='shop'>
-                   <Link className='shop-links'>SHOP NOW</Link>
-                </div>
-                
-                </div>
-                </div>
-                    
-                 </div>
-
-                 <div className ='new-arrival'>
-                     <Link className='nav-links'>
-                          LAUNCHES
-                     </Link>
-                 </div>
-
-                 <div className ='new-arrival'>
-                     <Link className='nav-links'>
-                          IN STORE ONLY
-                     </Link>
-                 </div> 
-
+            
+            <div className='wrapper'>
+              <ul className='nav-links'>
+                <li className='list'>
+                    <Link className='links'>NEW ARRIVAL</Link>
+                <ul className='drop-down'>
+                    <li className='list'><Link className='drop-links'>MEN</Link></li>
+                    <li className='list'><Link className='drop-links'>WOMEN ARRIVAL</Link></li>
+                    <li className='list'><Link className='drop-links'>SHOES</Link></li>
+                    <li className='list'><Link className='drop-links'>APPARELS</Link></li>
+                    <li className='list'><Link className='drop-link'>SHOP NOW</Link></li>
+                </ul>
+                </li>
+                <li className='list'>
+                    <Link className='links'>MEN</Link>
+                </li>
+                <li className='list'>
+                    <Link className='links'>WOMEN</Link>
+                </li>
+                <li className='list'>
+                    <Link className='links'>KIDS</Link>
+                </li>
+                <li className='list'>
+                    <Link className='links'>SALES</Link>
+                </li>
+                <li className='list'>
+                    <Link className='links'>BRANDS</Link>
+                </li>
+                <li className='list'>
+                    <Link className='links'>LAUNCHES</Link>
+                </li>
+                <li className='list'>
+                    <Link className='links'>IN STORE ONLY</Link>
+                </li>
+                </ul>
+            </div>
+            
         </SubHeaderContainer>
     )
 }
--- src/components/subheader/subheader.styles.jsx
+++ src/components/subheader/subheader.styles.jsx
@@ -5,9 +5,67 @@ import Women from '../../assets/wc.jpg';
 import Kids from '../../assets/newbalancee.jpg';
 import Brands from '../../assets/oc.jpg';
 
-
 export const SubHeaderContainer = styled.div`
-  position: absolute;
+  /* position: fixed;
+  z-index: 1; */
+  width: 98%;
+  margin: 0 auto;
+  
+
+  .wrapper {
+    width: 80%;
+  }
+
+  .nav-links {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .list {
+    list-style: none;
+  }
+
+  .links {
+    text-decoration: none;
+    color: black;
+  }
+
+  .drop-links {
+    width: 100%;
+    text-decoration: none;
+    color: black;
+    /* padding: 0 0 0 15px; */
+  }
+
+  .drop-link {
+    width: 100%;
+    color: black;
+    /* padding: 0 0 0 15px; */
+  }
+
+  .drop-down {
+    position: absolute;
+    z-index: 2;
+    background-color: white;
+    line-height: 45px;
+    width: 25%;
+    opacity: 0;
+    visibility: hidden;
+    transition: all 0.3s ease;
+  }
+
+  & :hover {
+    cursor: pointer;
+
+   .drop-down {
+       opacity: 1;
+       visibility: visible;
+   }
+
+  } 
+  /* position: absolute;
   width: 90%;
   height: 70vh;
   display: flex;
@@ -18,16 +76,19 @@ export const SubHeaderContainer = styled.div`
   
   @media screen and (max-width: 700px) { 
         position: fixed;
+        padding-top: 2em;
         z-index: 3;
         background-color: white;
         width: 100%;
+        overflow-x: scroll;
+        overflow-y: scroll;
+        top: 0;
         left: 0;
-        height: 100vh;
+        height: 100%;
         display: flex;
         flex-direction: column;
         transform: ${({open}) => open ? 'translateX(0)' : 'translateX(100%)'};
         transition: transform 0.3s ease-in-out;
-        overflow: scroll;
   }
 
 
@@ -53,15 +114,23 @@ export const SubHeaderContainer = styled.div`
       background-color: white;
       display: none;  
       z-index: 2;
+
+      @media screen and (max-width: 700px) { 
+        /* display: none; */
+        width: 100%;
+        height: 50%;
+        
+    }
   }
 
   .links-container {
     width: 35%;
+    border: 2px solid red;
 
 
     @media screen and (max-width: 700px) { 
         /* display: none; */
-      
+      width: 100%;
     }
   }
 
@@ -156,6 +225,6 @@ export const SubHeaderContainer = styled.div`
        justify-content: space-evenly;
    }
 
-  }
+  } */
 
 `;
