d177377af0902fe760caaaaa95caa90661a1af37
otonye <otonyeamie@gmail.com>
2021-04-09 13:35:16 +0100

    master

---
+++ src/pages/w-northface/w-northface.component.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { TreyfoilContainer } from './w-northface.styles';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+import {WomenCollections} from '../../redux/women/women.selector';
+import CollectionItems from '../../components/collection-items/collection-items.component';
+import RelatedProducts from '../../components/related-products/related-products.component';
+import RelatedImages from '../../components/related-images/related-images.component';
+import RecentlyViewed from '../../components/recently-viewed/recently-viewed.component';
+import RecentlyViewedImages from '../../components/recently-viewed-images/recently-viewed-images.component';
+
+
+
+const WnorthfacePage = ({sections}) => {
+  console.log(sections)
+
+      return ( 
+           <TreyfoilContainer>
+              <div className='left-side'>
+                  {
+                    sections.filter((item, id) => id === 1).map((section) => (
+                      <CollectionItems key={section.id} section={section}/>
+                    ))
+                  }
+              </div>
+
+              <br />
+              <div className='related-products'>
+                <RelatedProducts />
+              </div>
+              <br />
+              <div className='related-images'>
+                <RelatedImages />
+              </div>
+              <br />
+              <div className='recently-viewed'>
+                <RecentlyViewed />
+              </div>
+              <div className='recently-viewed-images'>
+                <RecentlyViewedImages />
+              </div>
+
+             
+            </TreyfoilContainer>
+             
+      );
+  };
+  
+  const mapStateToProps = createStructuredSelector({
+    sections: WomenCollections
+  })
+  
+ 
+  
+  export default connect(mapStateToProps)(WnorthfacePage);
--- src/pages/women-collection-arrival/women-collection-arrival.component.jsx
+++ src/pages/women-collection-arrival/women-collection-arrival.component.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { NewArrivalContainer } from './women-collection-arrival.styles';
+import { Route } from 'react-router-dom';
 import MenLink from '../../components/men-link-hero/men-link-hero.component';
 import WomenText from '../../components/women-text/women-text.component';
 import WomenCollections from '../../components/women-collections/women-collection-categories.component';
+import WadidasPage from '../w-adidas/w-adidas.component';
+import WnorthfacePage from '../w-northface/w-northface.component';
+import WfilaPage from '../w-fila/w-fila.component';
+import WhempesPage from '../w-hempes/w-hempes.component';
+import WdustPage from '../w-dust/w-dust.component';
+import WlondonPage from '../w-london/w-london.component';
 import Footer from '../../components/footer/footer.component';
 
 
 
-const WomenCollectionPage = () => {
+const WomenCollectionPage = ({match}) => {
     return(
         <NewArrivalContainer>
               <div className=''>
@@ -16,7 +23,14 @@ const WomenCollectionPage = () => {
                   <WomenText />
                   <br />
                   <br />
-                  <WomenCollections />
+                  {/* <WomenCollections /> */}
+                  <Route exact path={`${match.path}`} component={WomenCollections} />
+                  <Route path={`${match.path}/:womens-adidas-originals-adicolor-classics-cropped-track-jacket-black`} component={WadidasPage} />
+                  <Route path={`${match.path}/:womens-the-north-face-95-retro-denali-jacket-black`} component={WnorthfacePage} />
+                  <Route path={`${match.path}/:womens-fila-doro-puffer-jacket-white`} component={WfilaPage} />
+                  <Route path={`${match.path}/:womens-adidas-originals-windbreaker-hemp`} component={WhempesPage} />
+                  <Route path={`${match.path}/:adidas-originals0shopper-bag-dust-pearl`} component={WdustPage} />
+                  <Route path={`${match.path}/:womens-adidas-originals-her-studio-london-biker-pants`} component={WlondonPage} />
               </div> 
               <br />
             <br />
